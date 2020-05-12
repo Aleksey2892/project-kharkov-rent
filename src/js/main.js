c08ea3e7eaf85e31863b18781ebeb943f659c419
Aleksey <aleksey2892@gmail.com>
2020-05-12 22:20:31 +0300

    work with dynamic js width

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 $('.burger-menu').click(function() {
   $(this).toggleClass('burger-menu--active');
 });
+
+const titleWidth = document.querySelector('.block-title').clientWidth;
+console.log(`Получили значение ширины: ${titleWidth}`);
+
+const calc = titleWidth * 0.2;
+console.log(`Сделали манипуляции: ${calc}`);
+
+document.querySelector('.block-title-wrapper').style.width = `${calc}px`;
+
+console.log(
+  `Присвоили такой результат: ${
+    document.querySelector('.block-title-wrapper').style.width
+  }`,
+);
