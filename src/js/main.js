0505bc3055ccd60d9b4b4e45a08c8c31caf60f2c
Aleksey <aleksey2892@gmail.com>
2020-05-14 02:50:16 +0300

    fix js dynamic widh and height

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,11 @@ $('.burger-menu').click(function() {
   $(this).toggleClass('burger-menu--active');
 });
 
-const titleWidth = document.querySelector('.block-title').clientWidth;
-console.log(`Получили значение ширины: ${titleWidth}`);
+// dynamic width using js '1.0'
+const titleMargin = document.querySelector('.block-title').offsetLeft; // get 'margin'
+let titleWidth = document.querySelector('.block-title').clientWidth; // get 'width'
+titleWidth = titleWidth * 0.8 + titleMargin; // increase the width by the margin 'value'
+document.querySelector('.block-title-wrapper').style.width = `${titleWidth}px`; // assign 'width'
 
-const calc = titleWidth * 0.2;
-console.log(`Сделали манипуляции: ${calc}`);
-
-document.querySelector('.block-title-wrapper').style.width = `${calc}px`;
-
-console.log(
-  `Присвоили такой результат: ${
-    document.querySelector('.block-title-wrapper').style.width
-  }`,
-);
+const titleHeight = document.querySelector('.block-title').clientHeight + 50; // get 'height'
+document.querySelector('.block-title-wrapper').style.height = `${titleHeight}px`; // assign 'height'
